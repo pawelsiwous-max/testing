3d2472fd44908f5b38163c593db7dd6b2811f2c5
Pasha <pasha@example.com>
2025-12-15 01:45:48 +0200

    Add sarvise

---
+++ sw.js
@@ -0,0 +1,45 @@
+const CACHE_NAME = 'fake-dia-v1';
+
+const FILES_TO_CACHE = [
+  './',
+  './index.html',
+  './styles.css',
+  './manifest.json',
+  './icon-192.svg',
+  './icon-512.svg'
+];
+
+// install
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(FILES_TO_CACHE);
+    })
+  );
+  self.skipWaiting();
+});
+
+// activate
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.map(key => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      )
+    )
+  );
+  self.clients.claim();
+});
+
+// fetch
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
